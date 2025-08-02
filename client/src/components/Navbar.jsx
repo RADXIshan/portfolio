77bff66c14e2e40472f06027a498f8a2f746a938
Ishan Roy <ishanroy3118107@gmail.com>
2025-08-03 01:20:18 +0530

    Updated Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,46 @@
-import "../styles/Navbar.css"
-import { Link, Navigate } from 'react-router';
+import { useRef, useState } from "react"; 
+import "../styles/Navbar.css";
+import { Link } from 'react-router'; 
+import logo from "../assets/logo.png";
+import { Menu, X } from "lucide-react"; 
+import { gsap } from "gsap";
 
 const Navbar = () => {
+  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const navLinksRef = useRef(null);
+
+  const toggleMenu = () => {
+
+    gsap.to(navLinksRef.current, {
+      duration: 0.8,
+      height: isMenuOpen ? "0" : "100vh", 
+      ease: "power3.inOut",
+    });
+ 
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      <div className="logo">
-        <img src="#" alt="logo" />
-        <p>Ishan Roy</p>
+    <>
+      <nav>
+        <div className="logo">
+          <Link className="nav-link" to="#hero"><img src={logo} alt="logo" /></Link>
+        </div>
+        <div onClick={toggleMenu}>
+          {isMenuOpen ? <X className="menu-icon"/> : <Menu className="menu-icon"/>}
+        </div>
+      </nav>
+      
+      <div className="nav-links" ref={navLinksRef}>
+          <Link className="nav-link" to="#hero">Home</Link>
+          <Link className="nav-link" to="#about">About</Link>
+          <Link className="nav-link" to="#projects">Projects</Link>
+          <Link className="nav-link" to="#contact">Contact</Link>
       </div>
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
-      </div>
-    </nav>
+    </>
   )
 }
 
-export default Navbar
+export default Navbar;
