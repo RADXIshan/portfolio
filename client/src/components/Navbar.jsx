e9f4b6c0c2b2bb59739f10842eb9e823878318bb
Ishan Roy <ishanroy3118107@gmail.com>
2025-08-02 16:57:40 +0530

    Navbar changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+import "../styles/Navbar.css"
 import { Link, Navigate } from 'react-router';
 
 const Navbar = () => {
   return (
     <nav>
-      Navbar
+      <div className="logo">
+        <img src="#" alt="logo" />
+        <p>Ishan Roy</p>
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </nav>
   )
 }
