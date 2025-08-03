2ef3f9a9a0d68cdbd47231c5f46d09b7027d788a
Ishan Roy <ishanroy3118107@gmail.com>
2025-08-03 16:10:39 +0530

    Added Gsap to Contact Form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,45 +1,112 @@
-import "../styles/Contact.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import "../styles/Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Contact = () => {
+  const contactRef = useRef(null);
+
+  useEffect(() => {
+    const element = contactRef.current;
+
+    gsap.fromTo(
+      element,
+      { y: 100, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1.5,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: element,
+          start: "top 40%", 
+          end: "top 35%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+  }, []);
+
   return (
-    <div id='contact' className='contact'>
-        <div className='left-side'>
-          <h2>Get In Touch</h2>
-          <p>I would love to hear from you!</p>
-          <div className="contact-details">
-            <a className='social-link' href='https://www.linkedin.com/in/ishanroy-radx/' target='_blank' rel='noopener noreferrer' data-label="Linkedin">
-                    <FontAwesomeIcon icon={faLinkedin} />
-                </a>
-                <a className='social-link' href='https://github.com/RADXIshan' target='_blank' rel='noopener noreferrer' data-label="Github">
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>
-                <a className='social-link' href='mailto:ishanroy3118107@gmail.com' data-label="Gmail">
-                    <FontAwesomeIcon icon={faEnvelope} />
-                </a>
-                <a className='social-link' href="tel:+919007195462" data-label="Phone">
-                    <FontAwesomeIcon icon={faPhone} />
-                </a>
-          </div>
+    <div id="contact" className="contact" ref={contactRef}>
+      <div className="left-side">
+        <h2>Get In Touch</h2>
+        <p>I would love to hear from you!</p>
+        <div className="contact-details">
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/ishanroy-radx/"
+            target="_blank"
+            rel="noopener noreferrer"
+            data-label="Linkedin"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            className="social-link"
+            href="https://github.com/RADXIshan"
+            target="_blank"
+            rel="noopener noreferrer"
+            data-label="Github"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            className="social-link"
+            href="mailto:ishanroy3118107@gmail.com"
+            data-label="Gmail"
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+          <a
+            className="social-link"
+            href="tel:+919007195462"
+            data-label="Phone"
+          >
+            <FontAwesomeIcon icon={faPhone} />
+          </a>
         </div>
-        <div className="right-side">
-          <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Enter your name" required />
+      </div>
 
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" required />
+      <div className="right-side">
+        <form>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
 
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" rows="4" placeholder="Type your message here..." required></textarea>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+          />
 
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            placeholder="Type your message here..."
+            required
+          ></textarea>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
