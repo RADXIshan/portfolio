15d92b67981b47988f78732caed590ba7b0e28a0
Ishan Roy <ishanroy3118107@gmail.com>
2025-08-05 19:43:38 +0530

    Added mailing option

--- server/src/controllers/contactController.js
+++ server/src/controllers/contactController.js
@@ -1,3 +1,5 @@
+import nodemailer from 'nodemailer';
+
 export const mailContact = async(req, res) => {
   const { name, email, message } = req.body;
 
@@ -20,5 +22,45 @@ export const mailContact = async(req, res) => {
 
   console.log("Received:", { name, email, message });
 
-  res.status(200).json({ success: true, message: "Message sent successfully" });
+  const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.APP_PASSWORD,
+        },
+    });
+
+    const mailOptions = {
+        from: {
+            name: "Ishan Roy",
+            address: "trickster10ishan@gmail.com"
+        },
+        to: "ishanroy3118107@gmail.com",
+        subject: "Sent from Portfolio website",
+        text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nMessage: ${req.body.message}`,
+    };
+    const confirmationMail = {
+        from: {
+            name: "Ishan Roy",
+            address: "trickster10ishan@gmail.com"
+        },
+        to: req.body.email,
+        subject: "Portfolio Message Received",
+        text: `Hi ${req.body.name},\n\nThank you for reaching out to Ishan Roy. He will get back to you shortly.`,
+    };    
+
+    try {
+        await Promise.all([
+            transporter.sendMail(mailOptions),
+            transporter.sendMail(confirmationMail)
+        ]);        
+        res.status(200).json({ success: true, message: "Message sent successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to send message" });
+    }
+
+  
 }
